b80a522f1bd0f47af4fb3532daefaf35a62bbc3c
ABHINAV RAJ <rajabhinav734@gmail.com>
2026-02-20 15:43:31 +0530

    Update page.tsx

--- app/handler/[...stack]/page.tsx
+++ app/handler/[...stack]/page.tsx
@@ -5,5 +5,13 @@ export const dynamic = 'force-dynamic'
 
 export default function Handler(props: any) {
   console.log("Stack Handler accessed with props:", props)
-  return <StackHandler app={stackServerApp} {...props} />
+  
+  return (
+    // Is div ne form ko screen ke bilkul center mein kar diya hai
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <div className="w-full max-w-md">
+        <StackHandler app={stackServerApp} {...props} />
+      </div>
+    </div>
+  )
 }
