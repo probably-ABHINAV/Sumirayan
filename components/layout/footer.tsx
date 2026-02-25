c60c4c5af011fa67afa60c38c99956d1387747cf
ABHINAV RAJ <rajabhinav734@gmail.com>
2026-02-25 14:42:22 +0530

    Update footer.tsx

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -33,13 +33,13 @@ export function Footer() {
                 <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                 </svg>
-                contact@sumirayandesign.com
+                sumirayandesign@gmail.com
               </a>
               <a href="tel:+919876543210" className="hover:text-foreground transition-colors inline-flex items-center gap-2">
                 <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                +91 98765 43210
+                +91 89368 41201
               </a>
             </div>
 
@@ -48,7 +48,7 @@ export function Footer() {
               {[
                 {
                   name: "Instagram",
-                  url: "https://instagram.com/your_handle", // Replace with your link
+                  url: "https://www.instagram.com/sumirayan_design?igsh=Y3Y5eWU1OHRkaXI2", // Replace with your link
                   icon: "M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069z",
                 },
                 {
@@ -58,7 +58,7 @@ export function Footer() {
                 },
                 {
                   name: "Facebook",
-                  url: "https://facebook.com/your_page", // Replace with your link
+                  url: "https://www.facebook.com/share/1AgVQMPA3y/", // Replace with your link
                   icon: "M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z",
                 },
                 {
